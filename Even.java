35266837df4f7ee78a4c17052384f68e1aacf0c6
ARUNKUMAR <40732395+archuarun@users.noreply.github.com>
2018-07-14 10:47:33 +0530

    Odd or Even

---
+++ Even.java
@@ -0,0 +1,19 @@
+package ak;
+import java.util.Scanner;
+public class Even {
+	public static void main(String...args){
+		int i;
+		Scanner arun=new Scanner (System.in);
+		i=arun.nextInt();
+		if(i<=0){
+			System.out.println("Invalid");
+		}
+		else if(i%2==0){
+			System.out.println("even");
+		}
+		else{
+			System.out.println("odd");
+			arun.close();
+		}
+	}
+}
