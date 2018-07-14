a6a20504c6480a68399efcd4595e104d8469619a
ARUNKUMAR <40732395+archuarun@users.noreply.github.com>
2018-07-14 10:34:59 +0530

    (+) or (-) or ( 0)

---
+++ Positive.java
@@ -0,0 +1,20 @@
+package ak;
+import java.util.Scanner;
+public class Positive{
+	public static void main(String...args){
+	int i;
+	Scanner arun=new Scanner(System.in);
+	i=arun.nextInt();
+	if(i==0){
+		System.out.println("Zero");
+	}
+	else if(i<0){
+		System.out.println("Negative");
+	}
+		else{
+			System.out.println("Positive");
+		}
+	arun.close();
+	}
+}
+
