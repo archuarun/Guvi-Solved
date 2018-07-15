b41419de8ce7324ff801f96995ac50b0724aaba9
ARUNKUMAR <40732395+archuarun@users.noreply.github.com>
2018-07-15 10:23:40 +0530

    Alphabets Or Special Chars

---
+++ Alphabet.java
@@ -0,0 +1,15 @@
+package ak;
+import java.util.Scanner;
+public class Alphabet {
+	public static void main(String...args){
+		char ch;
+		Scanner arun=new Scanner(System.in);
+		ch=arun.next().charAt(0);
+		if(ch>=65&&ch<=90||ch>=97&&ch<=122){
+			System.out.println("Alphabets");
+		}
+		else{
+			System.out.println("invalid");
+	}
+	}
+}
