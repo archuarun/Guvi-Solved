ad5f970dc4884c72f607c7e727e4df20d6985d2e
ARUNKUMAR <40732395+archuarun@users.noreply.github.com>
2018-07-22 12:02:02 +0530

    Print Word N times

---
+++ Word.java
@@ -0,0 +1,14 @@
+package ak;
+import java.util.Scanner;
+public class Word {
+	public static void main(String...args){
+	String s="Hello";
+	int a,i;
+	Scanner arun=new Scanner(System.in);
+	a=arun.nextInt();
+	for(i=1;i<=a;i++){
+		System.out.println(s);
+	}
+
+}
+}
