6dfbc589f97e20a8bfbe9b8ee1f7013cc3acf4c7
ARUNKUMAR <40732395+archuarun@users.noreply.github.com>
2018-07-30 10:06:36 +0530

    Count Spaces in String

---
+++ Countspaces.java
@@ -0,0 +1,13 @@
+package ak;
+import java.util.Scanner;
+public class Countspaces {
+	public static void main(String...args){
+		String s;
+		Scanner arun=new Scanner(System.in);
+		s=arun.nextLine();
+		String[] s1=s.split(" ");
+		int l=s1.length-1;
+		System.out.println(l);
+	}
+
+}
