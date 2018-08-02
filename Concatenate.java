8480d75d8e182bcfb771999538e480ffedb2948c
ARUNKUMAR <40732395+archuarun@users.noreply.github.com>
2018-08-02 12:00:31 +0530

    Concatenate two String

---
+++ Concatenate.java
@@ -0,0 +1,14 @@
+package ak;
+
+import java.util.Scanner;
+
+public class Concatenate {
+	public static void main(String...args){
+		String s,s1;
+		Scanner arun=new Scanner(System.in);
+		s=arun.next();
+		s1=arun.next();
+		System.out.println(s+s1);
+	}
+
+}
