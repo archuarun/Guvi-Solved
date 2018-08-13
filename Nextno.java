eff0aba323e628256b89d3d065427f37f46b4365
ARUNKUMAR <40732395+archuarun@users.noreply.github.com>
2018-08-13 09:17:22 +0530

    Print next no

---
+++ Nextno.java
@@ -0,0 +1,11 @@
+package ak;
+import java.util.Scanner;
+public class Nextno {
+ 	public static void main(String...args){
+	int a;
+	Scanner arun=new Scanner(System.in);
+	a=arun.nextInt();
+	a=a+1;
+	System.out.println(a);
+	}
+        }
