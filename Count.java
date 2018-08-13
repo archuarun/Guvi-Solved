458b04227936f38ad32231bef8cafead2b149b01
ARUNKUMAR <40732395+archuarun@users.noreply.github.com>
2018-08-13 09:20:06 +0530

    Count an Integer

--- Count.java
+++ Count.java
@@ -2,11 +2,10 @@ package ak;
 import java.util.Scanner;
 public class Count {
 	public static void main(String...args){
-		int i,j;
+		String i,j;
 		Scanner arun=new Scanner(System.in);
-		i=arun.nextInt();
-		String s=String.valueOf(i);
-		j=s.length();
+		i=arun.nextLine();
+		j=i.length();
 		System.out.println(j);
 	arun.close();
 	}
